175a988f19a9952898b88752561c7973d3dd3bcf
Hassan <info.hassan.faisal@gmail.com>
2022-05-01 21:14:14 +0500

    Third

--- Main/my-blog/pages/index.js
+++ Main/my-blog/pages/index.js
@@ -2,6 +2,23 @@ import Head from 'next/head'
 import Script from 'next/script'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import social from '../social/Social.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+// import your icons
+import {
+  faGithub,
+  faTwitter,
+  faLinkedin,
+  faInstagram
+} from '@fortawesome/free-brands-svg-icons'
+library.add(
+  faGithub,
+  faTwitter,
+  faLinkedin,
+  faInstagram
+)
 
 export default function Home() {
   return (
@@ -12,15 +29,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Script src='/sc.js'  strategy='lazyOnload'>
-
-      </Script>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
          Personal <a href="https://ud.me/hassanfaisal.nft" target="_blank">Blog</a>
         </h1>
-
+        <div className={social.social}>
+        <FontAwesomeIcon className={social.twitter} icon={faTwitter} style={{width:'40px'}} />
+        <FontAwesomeIcon className={social.instagram} icon={faInstagram} style={{width:'40px'}}/>
+        <FontAwesomeIcon className={social.github} icon={faGithub} style={{width:'40px'}}/>
+        <FontAwesomeIcon className={social.linkedin} icon={faLinkedin} style={{width:'40px'}}/>
+        </div>
         <p className={styles.description}>
           Let Me Tell You What{' '}
           <code className={styles.code}><a>Boring</a> / <a>Excitement</a> / <a>Challenges</a></code>
@@ -55,19 +73,16 @@ export default function Home() {
             </p>
           </a>
         </div>
+
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+       
           Powered by{'  '}
-          <p className={styles.titlef}>
+          <h4 className={styles.titlef}>
          <a href="https://ud.me/hassanfaisal.nft" target="_blank">Hassan Faisal</a>
-        </p>
-        </a>
+        </h4>
+        
       </footer>
     </div>
   )
